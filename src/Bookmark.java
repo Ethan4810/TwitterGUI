ac712053c49fd7f7712144f467f41a0eacdaa993
jsnam <jsnam3379@gmail.com>
2022-11-16 22:44:05 +0900

    Added bookmark class.

---
+++ src/Bookmark.java
@@ -0,0 +1,7 @@
+
+public class Bookmark {
+	public int bookmark_id; // bmk_id
+	public int bookmark_post_id; // bmk_pst_id
+	public String bookmark_post_user_id; // bmk_pst_ur_id
+	public String bookmark_user_id; // bmk_usr_id
+}
